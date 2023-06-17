23468cfc5a3a87a17e7c6a15ebae98852b36907c
forhan96 <f.a.chowdhury19@gmail.com>
2023-06-17 22:45:02 +0600

    phone field

--- lib/app/modules/register/views/register_view.dart
+++ lib/app/modules/register/views/register_view.dart
@@ -29,154 +29,163 @@ class RegisterView extends GetView<RegisterController> with InputValidationMixin
 
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 40.w),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 40.w),
+          shrinkWrap: true,
+
+          // mainAxisAlignment: MainAxisAlignment.spaceAround,
+          // mainAxisSize: MainAxisSize.max,
+          children: [
+            SizedBox(
+              height: 56.h,
+            ),
+            Column(
               children: [
-                Column(
-                  children: [
-                    Image.asset(
-                      ImageAssets.logo,
-                      height: 120.h,
-                    ),
-                    SizedBox(
-                      height: 16.h,
-                    ),
-                    Text(
-                      'Register',
-                      style: GoogleFonts.alexandria(
-                        color: AppColors.primaryColor,
-                        fontSize: 30,
-                      ),
-                    ),
-                  ],
+                Image.asset(
+                  ImageAssets.logo,
+                  height: 120.h,
                 ),
-                Form(
-                  key: formKey,
-                  child: Column(
-                    children: [
-                      TextInput(
-                        controller: nameController,
-                        hintText: "Full Name",
-                        validator: (name) {
-                          if (isValidName(name ?? "")) {
-                            return null;
-                          } else {
-                            return 'Enter a valid email';
-                          }
-                        },
-                      ),
-                      SizedBox(
-                        height: 16.h,
-                      ),
-                      PhoneInput(
-                        onChanged: (phone) {
-                          countryCodeController.text = phone.countryCode;
-                          phoneController.text = phone.number;
-                        },
-                      ),
-                      SizedBox(
-                        height: 16.h,
-                      ),
-                      TextInput(
-                        controller: emailController,
-                        hintText: "Email Address",
-                        validator: (email) {
-                          if (isEmailValid(email ?? "")) {
-                            return null;
-                          } else {
-                            return 'Enter a valid email';
-                          }
-                        },
-                      ),
-                      SizedBox(
-                        height: 16.h,
-                      ),
-                      TextInput(
-                        controller: passwordController,
-                        hintText: "Password",
-                        validator: (password) {
-                          if (isNotEmpty(password ?? "")) {
-                            return null;
-                          } else {
-                            return 'Use uppercase, lowecase, digits & special characters';
-                          }
-                        },
-                        obscure: true,
-                      ),
-                      SizedBox(
-                        height: 16.h,
-                      ),
-                      TextInput(
-                        controller: confirmPasswordController,
-                        hintText: "Confirm Password",
-                        validator: (password) {
-                          if (isNotEmpty(password ?? "")) {
-                            return null;
-                          } else {
-                            return 'Use uppercase, lowecase, digits & special characters';
-                          }
-                        },
-                        obscure: true,
-                      ),
-                      SizedBox(
-                        height: 30.h,
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          if (!formKey.currentState!.validate()) {
-                            return;
-                          }
-                          Get.offNamedUntil(Routes.HOME, (route) => false);
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: AppColors.primaryColor,
-                          minimumSize: Size.fromHeight(40.h),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                        child: Text(
-                          "Register",
-                          style: GoogleFonts.alexandria(
-                            // color: AppColors.primaryColor,
-                            fontSize: 18,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                SizedBox(
+                  height: 16.h,
                 ),
-                RichText(
-                  text: TextSpan(
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: "Don't have an account?  ",
-                        style: GoogleFonts.alexandria(
-                          color: AppColors.primaryColor,
-                          fontSize: 14,
-                          fontWeight: FontWeight.w200,
-                        ),
-                      ),
-                      TextSpan(
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {
-                            Get.offAndToNamed(Routes.LOGIN);
-                          },
-                        text: "Login",
-                        style: GoogleFonts.alexandria(
-                          color: AppColors.primaryColor,
-                          fontSize: 14,
-                        ),
-                      ),
-                    ],
+                Text(
+                  'Register',
+                  style: GoogleFonts.alexandria(
+                    color: AppColors.primaryColor,
+                    fontSize: 30,
                   ),
                 ),
               ],
             ),
-          ),
+            SizedBox(
+              height: 70.h,
+            ),
+            Form(
+              key: formKey,
+              child: Column(
+                children: [
+                  TextInput(
+                    controller: nameController,
+                    hintText: "Full Name",
+                    validator: (name) {
+                      if (isValidName(name ?? "")) {
+                        return null;
+                      } else {
+                        return 'Enter a valid email';
+                      }
+                    },
+                  ),
+                  SizedBox(
+                    height: 16.h,
+                  ),
+                  PhoneInput(
+                    onChanged: (phone) {
+                      countryCodeController.text = phone.countryCode;
+                      phoneController.text = phone.number;
+                    },
+                  ),
+                  SizedBox(
+                    height: 16.h,
+                  ),
+                  TextInput(
+                    controller: emailController,
+                    hintText: "Email Address",
+                    validator: (email) {
+                      if (isEmailValid(email ?? "")) {
+                        return null;
+                      } else {
+                        return 'Enter a valid email';
+                      }
+                    },
+                  ),
+                  SizedBox(
+                    height: 16.h,
+                  ),
+                  TextInput(
+                    controller: passwordController,
+                    hintText: "Password",
+                    validator: (password) {
+                      if (isNotEmpty(password ?? "")) {
+                        return null;
+                      } else {
+                        return 'Use uppercase, lowecase, digits & special characters';
+                      }
+                    },
+                    obscure: true,
+                  ),
+                  SizedBox(
+                    height: 16.h,
+                  ),
+                  TextInput(
+                    controller: confirmPasswordController,
+                    hintText: "Confirm Password",
+                    validator: (password) {
+                      if (isNotEmpty(password ?? "")) {
+                        return null;
+                      } else {
+                        return 'Use uppercase, lowecase, digits & special characters';
+                      }
+                    },
+                    obscure: true,
+                  ),
+                  SizedBox(
+                    height: 30.h,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (!formKey.currentState!.validate()) {
+                        return;
+                      }
+                      Get.offNamedUntil(Routes.HOME, (route) => false);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: AppColors.primaryColor,
+                      minimumSize: Size.fromHeight(40.h),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Text(
+                      "Register",
+                      style: GoogleFonts.alexandria(
+                        // color: AppColors.primaryColor,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 70.h,
+            ),
+            RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(
+                children: <TextSpan>[
+                  TextSpan(
+                    text: "Don't have an account?  ",
+                    style: GoogleFonts.alexandria(
+                      color: AppColors.primaryColor,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w200,
+                    ),
+                  ),
+                  TextSpan(
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () {
+                        Get.offAndToNamed(Routes.LOGIN);
+                      },
+                    text: "Login",
+                    style: GoogleFonts.alexandria(
+                      color: AppColors.primaryColor,
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
